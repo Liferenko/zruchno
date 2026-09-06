3fd9b02809ee3aee3a90af562e71303229724953
Pavel Liferenko <liferenko.it@gmail.com>
2026-09-06 11:32:30 +0200

    fix: add bar padding for top-bottom (for default buttons and icons)

--- app/src/main/java/com/example/a10101010/ui/HomeScreen.kt
+++ app/src/main/java/com/example/a10101010/ui/HomeScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -133,6 +134,7 @@ fun HomeScreen(
         modifier = modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
+            .systemBarsPadding()
     ) {
         val boxWidth = maxWidth
         val boxHeight = maxHeight
